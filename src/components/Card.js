cc76c0d9eb7d040b013c8a7eaa493f7daeb3f442
caio oliveira <caio2601@gmail.com>
2024-05-14 16:23:37 -0300

    Second commit

---
+++ src/components/Card.js
@@ -0,0 +1,9 @@
+import React from "react";
+
+function card(props) {
+    return <div className={props.className ? `${props.className} card`: "card"}>
+        {props.children}
+    </div>
+}
+
+export default card
